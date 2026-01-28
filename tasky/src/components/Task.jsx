b68ed0bcf3bead6b82790130c4374f5a8e4406b8
Mariia Popovych <20107971@setu.ie>
2026-01-28 16:06:42 +0000

    Delete a task

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -9,6 +9,7 @@ const Task = (props) => {
             <p className="description">{props.description}</p>
             <p className="level">{props.level}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button onClick={props.deleteTask}className='deleteButton'>Delete</button>
 
         </div>
     )
